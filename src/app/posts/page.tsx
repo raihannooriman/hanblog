969cb7afd65f9cbe86c09f6fc2abc14c0ecb45eb
noorimanr@gmail.com <noorimanr@gmail.com>
2024-07-13 20:46:52 +0800

    feat: upvote

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,13 @@
+import PostsList from "@/components/PostsList";
+import { Suspense } from "react";
+
+export default async function Page() {
+  return (
+    <main className="text-center pt-16 px-5">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5">Post</h1>
+      <Suspense fallback="Loading...">
+        <PostsList />
+      </Suspense>
+    </main>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navLinks = [
+  {
+    href: "/",
+    label: "Home",
+  },
+  {
+    href: "/posts",
+    label: "Posts",
+  },
+];
+
+export default function Header() {
+  const pathname = usePathname();
+  return (
+    <header className="flex justify-between items-center py-4 px-7 border-b">
+      <Link href="/">
+        <h1 className="text-2xl font-bold">Hanblog</h1>
+      </Link>
+      <nav>
+        <ul className="flex gap-x-5 text-sm">
+          {navLinks.map((link) => (
+            <li key={link.href}>
+              <Link
+                className={`${
+                  pathname === link.href ? "text-zinc-900" : "text-zinc-400"
+                }`}
+                href={link.href}
+              >
+                {link.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
